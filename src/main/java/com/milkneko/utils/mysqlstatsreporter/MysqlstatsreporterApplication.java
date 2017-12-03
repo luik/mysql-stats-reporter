b910b5dffecb2a012ec05b7b776a0e206c2b2ff7
Luis Yucra <luis.yucra@gmail.com>
2017-12-03 09:20:54 -0500

    initial commit using web http://start.spring.io/

---
+++ src/main/java/com/milkneko/utils/mysqlstatsreporter/MysqlstatsreporterApplication.java
@@ -0,0 +1,12 @@
+package com.milkneko.utils.mysqlstatsreporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlstatsreporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlstatsreporterApplication.class, args);
+	}
+}
